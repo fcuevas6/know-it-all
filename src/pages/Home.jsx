234aad8a172b21778aa3bb19f18c08147ae8d126
Francisco Cuevas <124302532+fcuevas6@users.noreply.github.com>
2025-08-04 16:11:02 -0400

    import Tailwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,21 +6,24 @@ export default function Home() {
     const { setPlayer, player, startGame } = usePlayer()
 
     return (
-        <div>
-            <h1>Welcome!</h1>
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                startGame()}}>
-                <input
-                    type='text'
-                    role='Player Name'
-                    placeholder='Enter your name'
-                    value={player}
-                    onChange={(e) => setPlayer(e.target.value)}
-                    required
-                />
-                <button type='submit'>Start Game</button>
-            </form>
+        <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+            <div className='text-center p-6 bg-white rounded-2xl border-lime-400 border-4 outline-offset-[6px] outline-4 outline-yellow-300'>
+                <h1>Welcome!</h1>
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    startGame()}}
+                    className='flex flex-col items-center py-16'>
+                    <input
+                        type='text'
+                        role='Player Name'
+                        placeholder='Enter your name'
+                        value={player}
+                        onChange={(e) => setPlayer(e.target.value)}
+                        required
+                    />
+                    <button type='submit'>Start Game</button>
+                </form>
+            </div>
         </div>
     )
 }
